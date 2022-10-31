515f137c8fb21e3cdefc39709efb90e128dcc29c
Aditya Kaneriya <adityakaneriya@Adityas-Mac.local>
2022-10-31 21:42:02 +0530

    download functionality

--- src/components/MusicPlayer/Track.jsx
+++ src/components/MusicPlayer/Track.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {BsDownload} from "react-icons/bs"
 
 const Track = ({ isPlaying, isActive, activeSong }) => (
   <div className="flex-1 flex w-1/3 items-center justify-start">
@@ -17,9 +18,18 @@ const Track = ({ isPlaying, isActive, activeSong }) => (
       <p className="truncate text-white font-bold text-lg">
         {activeSong?.title ? activeSong?.title : "No active Song"}
       </p>
-      <p className="truncate text-gray-300">
-        {activeSong?.subtitle ? activeSong?.subtitle : "No active Song"}
-      </p>
+      <div className="flex flex-row gap-2">
+        <p className="truncate text-gray-300">
+          {activeSong?.subtitle ? activeSong?.subtitle : "No active Song"}
+        </p>
+        <a href={activeSong?.hub?.actions[1]?.uri} download = "my.mp3">
+          <BsDownload
+            color="white"
+            size={20}
+            className="sm:block cursor-pointer"
+          />
+        </a>
+      </div>
     </div>
   </div>
 );
